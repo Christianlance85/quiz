f15de3834e11dd3142de8e4afdef6fc5991e6071
Christian Lance <christianlance85@gmail.com>
2019-11-14 17:39:26 -0600

    Moved variables to the top

--- Assets/index.js
+++ Assets/index.js
@@ -1,134 +1,129 @@
+var time = document.getElementById('timer');
+var secondsLeft = 75;
+var startBtn = document.querySelector('#start');
+var startP = document.querySelector('.startPage');
+var one = document.querySelector('.one');
+var title = one.querySelector('h1');
+one.style.display = 'None';
+var i = 0;
+var buttons = one.querySelectorAll('button');
+var username = document.querySelector('#name');
+username.style.display = 'None';
+var submit = document.querySelector('#submit');
+var timerInterval;
+var finalScore = username.querySelector('h5');
+var list = document.querySelector('#highScoresList');
+var highScores = [];
+var uname = document.querySelector('input').value;
+
 var questions = [
-    {
-        title: "There are four parts to a box model. Which of these is NOT a member?",
-        choices: ["padding", "border", "position", "margin"],
-        answer: "position"
-    },
-    {
-        title: "What special character is used at the end of a function?",
-        choices: ["}", "!", "//", "-"],
-        answer: "}"
-    },
-    {
-        title: "A 'confirm' statement gives you a ______ result.",
-        choices: ["true", "array", "boolean", "squiggly bracketed"],
-        answer: "boolean"
-    },
-    {
-        title: "In this for loop, if you wanted add one each loop through you would use what in the blank?: for (var i = 0; i < vegetables.length; ___ )",
-        choices: ["i++", "+=", "/", "==="],
-        answer: "i++"
-    },
-    {
-        title: "In this array: var animals = [bats, birds, bees, pigs]; which one is the index of [4]? ",
-        choices: ["bats", "birds", "bees", "none of the above"],
-        answer: "none of the above"
-    },
-
-
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts'
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses'
+  },
+  {
+    title: 'Inside which HTML element do we put the javascript?',
+    choices: ['js', 'script', 'javascript', 'scripting'],
+    answer: 'script'
+  },
+  {
+    title: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+    choices: ['<script src= "xxx.js"', '<script href= "xxx.js"', '<script name= "xxx.js"', 'none of the above'],
+    answer: '<script src= "xxx.js"'
+  },
+  {
+    title: 'How do you write "Hello World" in an alert box?',
+    choices: ['msg("Hello, World");', 'msgBox("Hello, World");', 'alert("Hello, World");', 'alertBox("Hello, World");'],
+    answer: 'alert("Hello, World");'
+  }
 ];
 
-console.log(questions);
+function setTime() {
+  timerInterval = setInterval(function () {
+    secondsLeft--;
+    time.textContent = 'timer ' + secondsLeft;
 
-
-function startquiz() {
-
-
-  let btn = document.getElementById("startbtn");
-
-  btn.style.display = "none";
-
-  let landpg = document.getElementById("startpg");
-
-  landpg.style.display = "none";
-
-
-  let quiz = document.getElementById("quizboard");
-  quiz.style.display = "block";
-  writeButton()
-  timer()
-}
-let timeInterval
-let questionPostion = 0
-let timeLeft = questions.length * 15
-let domTime = document.getElementById('timer')
-
-function writeButton() {
-  document.getElementById('actualQuestion').textContent = questions[questionPostion].title
-  for (let i = 0; i < 4; i++) {
-    let currentButton = document.getElementById('btn' + i)
-    currentButton.textContent = questions[questionPostion].choices[i]
-    currentButton.onclick = function () { clicked(questions[questionPostion].choices[i]) }
-  }
-}
-function endGame() {
-  document.getElementById('actualQuestion').innerHTML = '<h2>Congratulations, you finished!</h2><br> Your final score: ' + timeLeft
-  document.getElementById('actualQuestion').setAttribute("style", "font-size: 24px;");
-  document.getElementById('answerBtns').innerHTML = 'Enter Initials <input type="text" name="initials" placeholder="Initials" id= "grabInitials"> <button id="submitInitials" type="button" onclick = "scoreResults() "> SUBMIT</button>'
-  document.getElementById('answerBtns').setAttribute("style", "border-radius: 10px; color:black; padding: 25px; font-size: 30px; text-align: center; font-family: sans-serif;");
- 
-}
-
-function clicked(answer) {
-  console.log(answer)
-  if (answer === questions[questionPostion].answer) {
-    console.log('correct')
-
-  }
-
-  else {
-    console.log('wrong')
-    timeLeft = timeLeft - 10;
-  }
-
-  questionPostion++
-  if (questionPostion === questions.length) {
-    clearInterval(timeInterval)
-    console.log('your score= ' + timeLeft)
-    endGame()
-  }
-  else {
-    writeButton()
-
-  }
-}
-
-function timer() {
-  timeInterval = setInterval(function () {
-    timeLeft--;
-
-    domTime.textContent = 'Seconds left: ' + timeLeft
-    if (timeLeft === 0) {
-      clearInterval(timeInterval)
-      console.log('out of time')
-      
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
     }
-
   }, 1000);
 }
 
 
-//this is the function that will create local storage for highscore
-function scoreResults() {
-  // grab user name
-  let user = document.getElementById("grabInitials").value;
-  // grab highscore
-  let score = timeLeft;
-  // grab past highscores and users names/Object
-  let scoreList = localStorage.getItem("scoreList");
-  // current score and name in object    
-  console.log(user);
-  console.log(score);
-  if (scoreList == null) {
-    scoreList = {};
-  }
-  else {
-    scoreList = JSON.parse(scoreList);
-  }
-  //save to local storage
-  scoreList[user] = score;
-  localStorage.setItem("scoreList", JSON.stringify(scoreList));
-  console.log(scoreList);
-  
-  displayScore()
-  }
+startBtn.addEventListener('click', function () {
+  one.style.display = 'block';
+  var q = questions[i];
+  title.innerHTML = q['title'];
+  var j = 0;
+  buttons.forEach(function (btntxt) {
+    btntxt.innerText = q['choices'][j];
+    j += 1;
+  });
+
+  setTime();
+  startP.style.display = 'none';
+  one.style.display = 'inline';
+});
+
+function displayQuestion() {
+  i++;
+  var q = questions[i];
+  title.innerHTML = q['title'];
+  var j = 0;
+  buttons.forEach(function (btntxt) {
+    btntxt.innerText = q['choices'][j];
+    j += 1;
+  });
+
+}
+
+function lastPage() {
+  clearInterval(timerInterval);
+  one.style.display = 'none';
+  username.style.display = 'block';
+  finalScore.textContent = 'Your final score is: ' + time.textContent;  
+}
+
+
+buttons.forEach(function (btn) {
+  btn.addEventListener('click', function () {
+    var q = questions[i];
+    if (i == questions.length - 1 && btn.textContent == q['answer']) {
+      time.textContent = secondsLeft - 0
+      lastPage()
+    }
+    if (i == questions.length - 1 && btn.textContent != q['answer']) {
+      time.textContent = secondsLeft - 15;
+      lastPage()
+    }
+
+    else if (btn.textContent != q['answer']) {
+      secondsLeft -= 15;
+      displayQuestion()
+    }
+
+    else if (btn.textContent == q['answer']) {
+      displayQuestion()
+    }
+
+
+  });
+});
+
+
+
+submitInitials.addEventListener('click', function () {
+  var uname = document.querySelector('input').value;
+  localStorage.setItem('name', JSON.stringify (uname));
+  localStorage.setItem('score', JSON.stringify (time.textContent));
+  var li = document.createElement("li");
+  li.textContent = uname + ' Score: ' + time.textContent;
+  list.appendChild(li);
+
+});
